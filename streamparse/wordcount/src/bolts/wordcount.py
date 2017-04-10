b93508c73bdb7a47efb317a2c14f841be503e917
Tyler Lugger <tyler.lugger@colorado.edu>
2017-04-09 22:29:22 -0600

    Updated bolt to add a word length and added a 1 second delay between each word

--- streamparse/wordcount/src/bolts/wordcount.py
+++ streamparse/wordcount/src/bolts/wordcount.py
@@ -1,4 +1,5 @@
 import os
+import time
 from collections import Counter
 
 from streamparse import Bolt
@@ -18,8 +19,10 @@ class WordCountBolt(Bolt):
 
     def process(self, tup):
         word = tup.values[0]
-        self._increment(word, 10 if word == "dog" else 1)
-        if self.total % 1000 == 0:
-            self.logger.info("counted [{:,}] words [pid={}]".format(self.total,
-                                                                    self.pid))
+        self._increment(word, len(word))
+        #if self.total % 1000 == 0:
+            #self.logger.info("counted [{:,}] words [pid={}]".format(self.total,self.pid))
+	self.logger.info("Word counted: %s, Number of letters: %d, Total Count: %d" %(word, len(word), self.total))
+
         self.emit([word, self.counter[word]])
+	time.sleep(1)
--- streamparse/wordcount/src/spouts/words.py
+++ streamparse/wordcount/src/spouts/words.py
@@ -2,12 +2,45 @@ from itertools import cycle
 
 from streamparse import Spout
 
+import tweepy
+from tweepy import OAuthHandler
+from tweepy.streaming import StreamListener
+from tweepy import Stream
+
+# enter the corresponding information from your Twitter application:
+# keep the quotes, replace this with your consumer key
+CONSUMER_KEY = 'GfMtCUKhLwaz340D2OoVPKBKE'
+# keep the quotes, replace this with your consumer secret key
+CONSUMER_SECRET = 'zuTtVGTkzmQlYXnHa0Cn9Mg3pW4tDGh2s5cvqMVxVRFB9CArdF'
+# keep the quotes, replace this with your access token
+ACCESS_KEY = '838120217368756228-gumly1EgzoYD25cwNY4Bx5lEjTA8mdJ'
+# keep the quotes, replace this with your access token secret
+ACCESS_SECRET = 'WRF3D3QsjiOjRS1rTO8Skm2WSyusrxgZl0XEUJ4XP1edb'
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+# api = tweepy.API(auth)
+#
+# for status in tweepy.Cursor(api.home_timeline).items(10):
+#     # Process a single status
+#     print(status.text)
+
+
+class MyListener(StreamListener):
+
+    def on_data(self, data):
+	self.data = data
+        return True
+
+    def on_error(self, status):
+        print(status)
+        return True
 
 class WordSpout(Spout):
     outputs = ['word']
 
     def initialize(self, stormconf, context):
         self.words = cycle(['dog', 'cat', 'zebra', 'elephant'])
+
 
     def next_tuple(self):
         word = next(self.words)

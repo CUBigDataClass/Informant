9e89bfc26e7ad565634e1a0c4c23cc3bfcc16516
Tyler Lugger <tyler.lugger@colorado.edu>
2017-04-21 16:37:55 -0600

    Successfully passed tweets through scripts using sockets.

--- streamparse/tweetprocessing/testPySocket.py
+++ streamparse/tweetprocessing/testPySocket.py
@@ -1,9 +1,32 @@
-from socketIO_client import SocketIO, BaseNamespace
+from socketIO_client import SocketIO
+import socket
+import sys
 
+# Create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Connect the socket to the port where the server is listening
+server_address = ('localhost', 5000)
+sock.connect(server_address)
+
+
+# Client handler
 def on_tweet(*args):
-    if args[0] is not None:
-        print(str(args[0].encode('utf-8')))
 
-socketIO = SocketIO('localhost', 4000)
-socketIO.on('tweet', on_tweet)
-socketIO.wait()
+    if args[0] is not None:
+        message = str(args[0].encode('utf-8'))
+
+        try:
+            # Send data
+            sock.sendall(message)
+        except:
+            pass
+
+
+# Client setup
+socketIOclient = SocketIO('localhost', 4000)
+print "Setting up client"
+socketIOclient.on('tweet', on_tweet)
+socketIOclient.wait()
+
+sock.close()

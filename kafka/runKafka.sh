ab0739059edefe78a55b6ea3bc9e9bfb5b1c57e4
Michael Xiao <mixi3377@colorado.edu>
2017-04-25 23:13:33 -0600

    Added bash script to run kafka

---
+++ kafka/runKafka.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+bin/zookeeper-server-start.sh config/zookeeper.properties &
+bin/kafka-server-start.sh config/server.properties &
---
+++ kafka/stopKafka.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+bin/kafka-server-stop.sh 
+bin/zookeeper-server-stop.sh
